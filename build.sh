f20c857bcacbb06c855f5d04a317540b0fabcd33
糖小贤 <127175984+xiaoxian8@users.noreply.github.com>
2026-02-07 17:43:26 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -20,32 +20,26 @@ args=(-j4
 	DTC=dtc
 	DEPMOD=depmod
 	DTC_OVERLAY_TEST_EXT=ufdt_apply_overlay)
-	
-#清理旧的构建
-make ${args[@]} mrproper
 
 #定义默认配置
 make ${args[@]} gauguin_kali_defconfig
-make ${args[@]} menuconfig
 
 #开始编译
-make ${args[@]}
+make ${args[@]} Image.gz-dtb dtbo.img
 
 #生成modules_install
 make ${args[@]} INSTALL_MOD_PATH=modules modules_install
 
 # 拷贝 Image 和 dtbo.img 到当前目录
-cp $(find out -type f \( -name "Image" -o -name "dtbo.img" \)) ./
+cp $(find out -type f \( -name "Image.gz-dtb" -o -name "dtbo.img" \)) ./
 
 #移动到 Anykernel3
-mv -v Image AnyKernel3/Image
+mv -v Image.gz-dtb AnyKernel3/Image.gz-dtb
 mv -v dtbo.img AnyKernel3/dtbo.img
-rm Image
 
 #合成DTB
-cat $(find out/arch/arm64/boot/dts/vendor/qcom/ -type f -name "*.dtb") > AnyKernel3/dtb
+#cat $(find out/arch/arm64/boot/dts/vendor/qcom/ -type f -name "*.dtb") > AnyKernel3/dtb
 
 #打包成 flashable zip
 cd AnyKernel3
 zip -r9v ../out/kernel.zip *
-rm Image dtbo.img dtb

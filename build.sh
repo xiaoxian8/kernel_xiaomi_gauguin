666ce1442328a056db0fa0e86d59c0aafaf3e819
糖小贤 <127175984+xiaoxian8@users.noreply.github.com>
2026-02-07 21:46:27 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -25,16 +25,16 @@ args=(-j$(nproc --all)
 make ${args[@]} gauguin_kali_defconfig
 
 #开始编译
-make ${args[@]} Image.gz-dtb dtbo.img modules
+make ${args[@]} Image-dtb dtbo.img modules
 
 #生成modules_install
 make ${args[@]} INSTALL_MOD_PATH=modules modules_install
 
 # 拷贝 Image 和 dtbo.img 到当前目录
-cp $(find out -type f \( -name "Image.gz-dtb" -o -name "dtbo.img" \)) ./
+cp $(find out -type f \( -name "Image-dtb" -o -name "dtbo.img" \)) ./
 
 #移动到 Anykernel3
-mv -v Image.gz-dtb AnyKernel3/Image.gz-dtb
+mv -v Image-dtb AnyKernel3/Image-dtb
 mv -v dtbo.img AnyKernel3/dtbo.img
 
 #合成DTB
